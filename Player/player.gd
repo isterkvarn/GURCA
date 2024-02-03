0a076c6ef462114addc36a2a6f02a09f5febef92
Ludde12345 <ludvig.klein@live.se>
2024-02-03 11:15:23 +0100

    idk

--- Player/player.gd
+++ Player/player.gd
@@ -22,7 +22,15 @@ func _physics_process(delta):
 	
 		# Check for launch input
 		if Input.is_action_just_pressed("launch"):
-			launch(Vector3(1, 1, 0))
+			var player_pos = global_transform.origin
+			var mouse_pos = get_viewport().get_mouse_position()
+			mouse_pos = get_viewport().get_camera_3d().project_ray_origin(mouse_pos)
+			print(player_pos)
+			print(mouse_pos)
+			var launch_vector = Vector3(mouse_pos.x,mouse_pos.y,0) - player_pos
+			
+			
+			launch(launch_vector.normalized())
 
 	move_and_slide()
 	
