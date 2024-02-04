8d512ef746c3e70395c1affacacbb25207c175f4
Dethcrvsh <fabianfabbe@hotmail.se>
2024-02-04 14:23:04 +0100

    Made an override for the start

--- StartScreen/start.gd
+++ StartScreen/start.gd
@@ -3,6 +3,8 @@ extends Node3D
 
 @onready var map_scene = preload("res://Maps/map.tscn")
 
+var active = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -14,6 +16,14 @@ func _process(delta):
 
 
 func _input(event):
-	if not event is InputEventMouseMotion:
+	if event is InputEventKey:
+		var key_event = event as InputEventKey
+		if key_event.keycode == 88 and not key_event.pressed:
+			active = not active
+			return
+		elif key_event.keycode == 88:
+			return
+
+	if not event is InputEventMouseMotion and active:
 		get_tree().root.add_child(map_scene.instantiate())
 		get_node("/root/Start").free()
